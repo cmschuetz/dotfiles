a87e5ac5da185ddb481a01c483f80288b2c6d759
cmschuetz <cmschuetz12@gmail.com>
2015-07-13 21:03:46 -0500

    added screenlayout

---
+++ screenlayout/.screenlayout/monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --off --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal --primary --output DVI-D-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-0 --off
